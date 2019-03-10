2a95bde2ccf440f2b3eb4a4fd9b179b033d54097
h_type <din.ranger@yandex.ru>
2019-03-10 20:34:35 +0500

    Add hot module reload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import { h, render, Component } from 'preact';
 
 class Clock extends Component {
+    constructor() {
+        super();
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount() {
+        setInterval(() => this.setState({ date: new Date() }), 1000);
+    }
+
     render() {
-        const time = new Date().toLocaleTimeString();
-        return <span>{time}</span>;
+        return <span>Time: {this.state.date.toLocaleTimeString()}</span>;
     }
 }
 
-render(<Clock />, document.body);
+let root;
+
+const initialize = () => {
+    root = render(<Clock />, document.body, root);
+};
+
+initialize();
+
+if (module.hot) {
+    module.hot.accept('./', () => requestAnimationFrame(initialize));
+}
