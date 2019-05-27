ccb44bb49b1b656d9b0ddd494fc5c5b037d8e9c4
h_type <din.ranger@yandex.ru>
2019-05-27 20:55:04 +0500

    Add my solution page mock

--- src/components/pages/MySolutions/index.jsx
+++ src/components/pages/MySolutions/index.jsx
@@ -1,9 +1,22 @@
 import {h} from 'preact';
 
+import Paragraph from 'components/common/Paragraph';
+import PageContainer from 'components/common/PageContainer';
+import PageHeader from './components/PageHeader';
+import Title from './components/Title';
+
+import style from './style.scss';
+
 const MySolutions = () => (
-    <div>
-        <h1>My solutions</h1>
-    </div>
+    <PageContainer>
+        <PageHeader />
+        <Title href='#' className={style['my-solutions__title']}>
+            First solution
+        </Title>
+        <Paragraph>
+            Description of first solution.
+        </Paragraph>
+    </PageContainer>
 );
 
 export default MySolutions;
