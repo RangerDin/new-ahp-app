71d08a71f2c6b216c818939dafaa699bb6941e05
h_type <din.ranger@yandex.ru>
2019-06-01 19:07:28 +0500

    Fix consistency ratio table

--- src/components/pages/Home/components/ConsistencyTable/index.jsx
+++ src/components/pages/Home/components/ConsistencyTable/index.jsx
@@ -5,19 +5,19 @@ import style from './style.scss';
 
 const ConsistencyTable = () => (
     <Fragment>
-        <Label className={style.result__header}>Consistency</Label>
+        <Label className={style.result__header}>Consistency ratio</Label>
         <table className={style.result__table}>
             <tr>
                 <th>Matrix</th>
-                <th>Consistency ratio</th>
+                <th>Value</th>
             </tr>
             <tr>
                 <td>parameters</td>
-                <td>objects by parameter "5"</td>
+                <td>0.28</td>
             </tr>
             <tr>
+                <td>objects by parameter "Alpha romero hru 15 v8"</td>
                 <td>0.72</td>
-                <td>0.28</td>
             </tr>
         </table>
     </Fragment>
