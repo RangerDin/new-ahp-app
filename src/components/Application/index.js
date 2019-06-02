5d6054ad87efad64c6da303b3340eb6bcb01e82b
h_type <din.ranger@yandex.ru>
2019-06-02 19:26:27 +0500

    Add menu toggling

--- src/components/Application/index.js
+++ src/components/Application/index.js
@@ -1,22 +1,22 @@
+import {h} from 'preact';
 import Router from 'preact-router';
-import {h, Component} from 'preact';
 
 import Header from 'components/common/Header';
 import Home from 'components/pages/Home';
 import MySolutions from 'components/pages/MySolutions';
 import AboutMethod from 'components/pages/AboutMethod';
 
-export default class Application extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <Router>
-                    <Home path='/' />
-                    <MySolutions path='/solutions/my' />
-                    <AboutMethod path='/about' />
-                </Router>
-            </div>
-        );
-    }
-}
+const Application = () => {
+    return (
+        <div>
+            <Header />
+            <Router>
+                <Home path='/' />
+                <MySolutions path='/solutions/my' />
+                <AboutMethod path='/about' />
+            </Router>
+        </div>
+    );
+};
+
+export default Application;
--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,4 +1,5 @@
 import {h} from 'preact';
+import {useState} from 'preact/hooks';
 
 import Logo from 'components/common/Logo';
 import MenuToggler from 'components/common/MenuToggler';
@@ -6,12 +7,19 @@ import Menu from 'components/common/Menu';
 
 import styles from './style.scss';
 
-const Header = ({isMenuOpen}) => {
+const Header = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
     return (
         <header className={styles.header}>
             <Logo />
-            <MenuToggler />
-            <Menu isOpen />
+            <MenuToggler
+                isOpen={isMenuOpen}
+                onClick={() => {
+                    setMenuOpen(!isMenuOpen);
+                }}
+            />
+            <Menu isOpen={isMenuOpen} />
         </header>
     );
 };
