2256d57c3186d580285586443a32f14f5346cba9
h_type <din.ranger@yandex.ru>
2019-06-30 17:49:20 +0500

    Add solutions loading from localStorage

--- src/utils/saving/localhost.js
+++ src/utils/saving/localhost.js
@@ -1,7 +1,15 @@
+const SOLUTION_PREFIX = 'solution_';
+
 export function saveSolutionToLocalhost(solution) {
-    localStorage.setItem(solution.question, JSON.stringify(solution));
+    localStorage.setItem(`${SOLUTION_PREFIX}${solution.question}`, JSON.stringify(solution));
 }
 
 export function loadSolutionFromLocalhost(question) {
-    return JSON.parse(localStorage.getItem(question));
+    return JSON.parse(localStorage.getItem(`${SOLUTION_PREFIX}${question}`));
+}
+
+export function loadAllSolutionsFromLocalhost() {
+    return Object.keys(localStorage)
+        .filter((key) => key.startsWith(SOLUTION_PREFIX))
+        .map((key) => JSON.parse(localStorage[key]));
 }
