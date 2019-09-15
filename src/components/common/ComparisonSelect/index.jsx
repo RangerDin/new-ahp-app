c52f50d75c1f829b144fef7cecef0d3188c6cbaa
h_type <din.ranger@yandex.ru>
2019-09-15 22:35:56 +0500

    Fix comparison select in comparison list

--- src/components/common/ComparisonSelect/index.jsx
+++ src/components/common/ComparisonSelect/index.jsx
@@ -1,22 +1,28 @@
 import {h} from 'preact';
 
-import {COMPARISON_LABELS, COMPARISON_VALUES} from 'constants/comparisons';
+import {
+    COMPARISON_LABELS,
+    COMPARISON_VALUES,
+    SHORT_COMPARISON_PRECISION,
+    SHORT_COMPARISON_VALUES_TO_LONG,
+} from 'constants/comparisons';
 import Select from '../Select';
 import cn from 'utils/classnames';
 import style from './style.scss';
 
 const options = Object.keys(COMPARISON_VALUES).map((key) => ({
     label: COMPARISON_LABELS[COMPARISON_VALUES[key]],
-    value: COMPARISON_VALUES[key],
+    value: COMPARISON_VALUES[key].toFixed(SHORT_COMPARISON_PRECISION),
 }));
 
 const ComparisonSelect = ({className, value, onChange}) => (
     <Select
         className={cn(className, style['comparison-select'])}
-        value={value}
+        value={value.toFixed(SHORT_COMPARISON_PRECISION)}
         options={options}
         onChange={(event) => {
-            const comparisonValue = parseFloat(event.target.value);
+            const comparisonValue =
+                SHORT_COMPARISON_VALUES_TO_LONG[event.target.value];
 
             onChange(comparisonValue);
         }}
