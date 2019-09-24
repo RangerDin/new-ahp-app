d6fedef69b1487bd9f8d6a98c7f14a0b10379a3d
h_type <din.ranger@yandex.ru>
2019-09-24 17:38:40 +0500

    Fix theme toggler

--- src/constants/themes.js
+++ src/constants/themes.js
@@ -1,5 +1,5 @@
-export const LIGHT_THEME = 'menu.theme-light';
-export const DARK_THEME = 'menu.theme-dark';
+export const LIGHT_THEME = 'menu.theme-dark';
+export const DARK_THEME = 'menu.theme-light';
 export const DEFAULT_THEME = LIGHT_THEME;
 
 const LIGHT_THEME_COLORS = {
