7706ec59c2fdced1842644f77cf793654c3039b3
h_type <din.ranger@yandex.ru>
2019-10-03 03:31:41 +0500

    Add non-unique name errors

--- src/components/pages/Home/components/NameInputs/index.jsx
+++ src/components/pages/Home/components/NameInputs/index.jsx
@@ -11,7 +11,6 @@ import {OpacityAnimation} from 'components/common/OpacityAnimation';
 const NameInputs = ({
     className,
     names,
-    error,
     labelText,
     inputPlaceholder,
     hasDeleteButton,
@@ -20,36 +19,57 @@ const NameInputs = ({
     onNameChange,
     onNameDelete,
     onNameAdd,
-}) => (
-    <div className={cn(className, style['object-names'])}>
-        <Label className={style['object-names__label']}>{labelText}</Label>
-        <TransitionGroup>
-            {names.map((name, index) => (
-                <OpacityAnimation key={index}>
-                    <NameInput
-                        value={name}
-                        onChange={(event) =>
-                            onNameChange(index, event.target.value)
-                        }
-                        placeholder={inputPlaceholder}
-                        hasDeleteButton={hasDeleteButton}
-                        onDelete={() => {
-                            onNameDelete(index);
-                        }}
-                        error={error}
-                    />
-                </OpacityAnimation>
-            ))}
-        </TransitionGroup>
-        {hasAddButton && (
-            <Button
-                className={style['object-names__button']}
-                onClick={onNameAdd}
-            >
-                {addButtonText}
-            </Button>
-        )}
-    </div>
-);
+    emptyNameError,
+    nonUniqueNameError,
+}) => {
+    const nameMap = names.reduce((map, name) => {
+        map[name] = name in map ? map[name] + 1 : 1;
+
+        return map;
+    }, {});
+    const getNameError = (name) => {
+        if (!name) {
+            return emptyNameError;
+        }
+
+        if (nameMap[name] > 1) {
+            return nonUniqueNameError;
+        }
+
+        return '';
+    };
+
+    return (
+        <div className={cn(className, style['object-names'])}>
+            <Label className={style['object-names__label']}>{labelText}</Label>
+            <TransitionGroup>
+                {names.map((name, index) => (
+                    <OpacityAnimation key={index}>
+                        <NameInput
+                            value={name}
+                            onChange={(event) =>
+                                onNameChange(index, event.target.value)
+                            }
+                            placeholder={inputPlaceholder}
+                            hasDeleteButton={hasDeleteButton}
+                            onDelete={() => {
+                                onNameDelete(index);
+                            }}
+                            error={getNameError(name)}
+                        />
+                    </OpacityAnimation>
+                ))}
+            </TransitionGroup>
+            {hasAddButton && (
+                <Button
+                    className={style['object-names__button']}
+                    onClick={onNameAdd}
+                >
+                    {addButtonText}
+                </Button>
+            )}
+        </div>
+    );
+};
 
 export default NameInputs;
