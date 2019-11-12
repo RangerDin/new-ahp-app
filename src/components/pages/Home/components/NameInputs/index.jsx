e48e78d6e4561ffd4e1c2fc015cbbd9a0bb26b2e
h_type <din.ranger@yandex.ru>
2019-11-13 03:11:47 +0500

    Refactor NameInputs

--- src/components/pages/Home/components/NameInputs/index.jsx
+++ src/components/pages/Home/components/NameInputs/index.jsx
@@ -8,6 +8,23 @@ import style from './style.scss';
 import {TransitionGroup} from 'react-transition-group';
 import {OpacityAnimation} from 'components/common/OpacityAnimation';
 
+const getNameError = (
+    name,
+    emptyNameError,
+    nonUniqueNameError,
+    isNameDuplicated
+) => {
+    if (!name) {
+        return emptyNameError;
+    }
+
+    if (isNameDuplicated(name)) {
+        return nonUniqueNameError;
+    }
+
+    return '';
+};
+
 const NameInputs = ({
     className,
     names,
@@ -22,50 +39,41 @@ const NameInputs = ({
     emptyNameError,
     nonUniqueNameError,
     isNameDuplicated,
-}) => {
-    const getNameError = (name) => {
-        if (!name) {
-            return emptyNameError;
-        }
-
-        if (isNameDuplicated(name)) {
-            return nonUniqueNameError;
-        }
-
-        return '';
-    };
-
-    return (
-        <div className={cn(className, style['object-names'])}>
-            <Label className={style['object-names__label']}>{labelText}</Label>
-            <TransitionGroup>
-                {names.map((name, index) => (
-                    <OpacityAnimation key={index}>
-                        <NameInput
-                            value={name}
-                            onChange={(event) =>
-                                onNameChange(index, event.target.value)
-                            }
-                            placeholder={inputPlaceholder}
-                            hasDeleteButton={hasDeleteButton}
-                            onDelete={() => {
-                                onNameDelete(index);
-                            }}
-                            error={getNameError(name)}
-                        />
-                    </OpacityAnimation>
-                ))}
-            </TransitionGroup>
-            {hasAddButton && (
-                <Button
-                    className={style['object-names__button']}
-                    onClick={onNameAdd}
-                >
-                    {addButtonText}
-                </Button>
-            )}
-        </div>
-    );
-};
+}) => (
+    <div className={cn(className, style['object-names'])}>
+        <Label className={style['object-names__label']}>{labelText}</Label>
+        <TransitionGroup>
+            {names.map((name, index) => (
+                <OpacityAnimation key={index}>
+                    <NameInput
+                        value={name}
+                        onChange={(event) =>
+                            onNameChange(index, event.target.value)
+                        }
+                        placeholder={inputPlaceholder}
+                        hasDeleteButton={hasDeleteButton}
+                        onDelete={() => {
+                            onNameDelete(index);
+                        }}
+                        error={getNameError(
+                            name,
+                            emptyNameError,
+                            nonUniqueNameError,
+                            isNameDuplicated
+                        )}
+                    />
+                </OpacityAnimation>
+            ))}
+        </TransitionGroup>
+        {hasAddButton && (
+            <Button
+                className={style['object-names__button']}
+                onClick={onNameAdd}
+            >
+                {addButtonText}
+            </Button>
+        )}
+    </div>
+);
 
 export default NameInputs;
