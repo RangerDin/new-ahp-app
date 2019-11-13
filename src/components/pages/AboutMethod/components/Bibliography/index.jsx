b9ef041ad71680a52884a39151e26dba2f9bf792
h_type <din.ranger@yandex.ru>
2019-11-13 17:02:38 +0500

    Add key to Bibliography

--- src/components/pages/AboutMethod/components/Bibliography/index.jsx
+++ src/components/pages/AboutMethod/components/Bibliography/index.jsx
@@ -3,7 +3,10 @@ import {Paragraph} from '../Paragraph';
 import {OL} from '../OL';
 import {SectionHeader} from '../SectionHeader';
 import {CONTENT_ANCHOR} from '../../constants/contents';
-import {BIBLIOGRAPHY_LIST, BIBLIOGRAPHY_ANCHOR_PREFIX} from '../../constants/bibliography';
+import {
+    BIBLIOGRAPHY_LIST,
+    BIBLIOGRAPHY_ANCHOR_PREFIX,
+} from '../../constants/bibliography';
 import {LI} from '../LI';
 import {useContext} from 'preact/hooks';
 import {TranslationContext} from 'utils/useTranslation';
@@ -16,12 +19,11 @@ export const Bibliography = () => {
             <SectionHeader anchor={CONTENT_ANCHOR.BIBLIOGRAPHY}>
                 {t('about.bibliography.title')}
             </SectionHeader>
-            <Paragraph>
-                {t('about.bibliography.literature-was-used')}
-            </Paragraph>
+            <Paragraph>{t('about.bibliography.literature-was-used')}</Paragraph>
             <OL>
                 {BIBLIOGRAPHY_LIST.map((item) => (
-                    <LI><a id={`${BIBLIOGRAPHY_ANCHOR_PREFIX}${item.order}`} />
+                    <LI key={item.order}>
+                        <a id={`${BIBLIOGRAPHY_ANCHOR_PREFIX}${item.order}`} />
                         {t(item.text)}
                     </LI>
                 ))}
